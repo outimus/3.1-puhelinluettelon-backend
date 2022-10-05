386bf63a56a278b5482de85c51bd44ecbf51b091
Outi Mustonen <outimus@gmail.com>
2022-10-05 14:29:07 +0300

    added eslint and problems fixed

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
-const morgan = require("morgan")
-const cors = require("cors")
-const { response } = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+/*const { response } = require('express')*/
 require('dotenv').config()
-const Person = require("./models/person")
+const Person = require('./models/person')
 const { default: mongoose } = require('mongoose')
 const { db } = require('./models/person')
+
+console.log(db)
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -16,29 +18,27 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-morgan.token("body", (req) => JSON.stringify(req.body))
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
-app.use(morgan("tiny"))
+app.use(morgan('tiny'))
 app.use(cors())
 
 const url = process.env.MONGODB_URI
-mongoose.connect(url).then(result => {
-  console.log("connected to MongoDB")
-})
-.catch((error) => {
-  console.log("error connecting to MongoDB: ", error.message)
-})
+mongoose.connect(url).then(console.log('connected to MongoDB'))
+  .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 if (!process.argv[3]) {
-  console.log("phonebook:")
+  console.log('phonebook:')
   Person.find({}).then(result => {
-      result.forEach(person => {
-        console.log(person.name, person.number)
-      })
+    result.forEach(person => {
+      console.log(person.name, person.number)
     })
+  })
 }
 
 const addedName = process.argv[3]
@@ -46,12 +46,10 @@ const addedNr = process.argv[4]
 
 if (process.argv.length > 3) {
   const person = new Person({
-      name: process.argv[3],
-      number: process.argv[4],    
+    name: process.argv[3],
+    number: process.argv[4],
   })
-  person.save().then(result => {
-      console.log(`added ${addedName} number ${addedNr} to phonebook`)
-    })
+  person.save().then(console.log(`added ${addedName} number ${addedNr} to phonebook`))
 }
 
 /*Person.find({}).then(result => {
@@ -60,61 +58,53 @@ if (process.argv.length > 3) {
   })
   mongoose.connection.close()
 })*/
-
-
 const tunniste = (min, max) => {
   return (
     Math.round(Math.random() * (max - min) + min)
   )
 }
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
 })
-
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
+    .then(response.status(204).end())
     .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
-  const määrä = 0 // Miten saan määrän muuttujaan?
+  const määrä = 0             // Miten saan määrän muuttujaan?
   let pvm = new Date()
   res.send(`<h4>Phonebook has info for ${määrä} people<h4/><h4>${pvm}<h4/>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id)
-  .then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
     if (person) {
       response.send(person.number)
     } else {
       response.status(404).end()
     }
-  })
-  .catch(error => next(error))
+  }).catch(error => next(error))
 })
 
-app.post('/api/persons', morgan(":body"), (request, response, next) => {
+app.post('/api/persons', morgan(':body'), (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
     return response.status(400).json({
-      error: "name is missing"
+      error: 'name is missing'
     })
   }
   if (!body.number) {
     return response.status(400).json({
-      error: "number is missing"
+      error: 'number is missing'
     })
   }
-  // Tämä ei toimi. "body.map" on väärin.
-  /*if (body.map(x => x.name === body.name).includes(true)) {
+  // Tämä ei toimi (db.map >> on väärin)
+  /*if (db.map(x => x.name === body.name).includes(true)) {
     return response.status(400).json({
       error: 'name must be unique'
     })
@@ -126,8 +116,7 @@ app.post('/api/persons', morgan(":body"), (request, response, next) => {
   })
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
-  .catch(error => next(error))
+  }).catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -139,9 +128,9 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({error: "malformatted id"})
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
   next(error)
--- models/person.js
+++ models/person.js
@@ -1,25 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
-const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minlenght: 3,
-      required: true
-    },
-    number: {
-      type: String,
-      required: true
-    } 
+const personSchema = new mongoose.Schema( {
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-const Person = mongoose.model('Person', personSchema)
-
+/*const Person = mongoose.model('Person', personSchema)*/
 module.exports = mongoose.model('Person', personSchema)
